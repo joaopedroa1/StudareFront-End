d43907f8a78a584ab9536f8c90a5b5a46dfe2fe8
joaopedroa1 <joaolpq@yahoo.com>
2024-09-06 10:56:15 -0300

    feat: add login button

--- src/components/Cadastro/index.jsx
+++ src/components/Cadastro/index.jsx
@@ -1,5 +1,6 @@
 import './style.css'
 import estudante from '../../assets/estudante.svg'
+import { Link } from 'react-router-dom';
 
 export const Cadastro = () =>{
     return(
@@ -8,10 +9,11 @@ export const Cadastro = () =>{
        <div className = 'invisivi'></div>
         <div className="introducao">
             <h1 className='criarContaTexto'>Crie sua conta</h1>
-            <h1 className='insiraDadosTexto'>Insira seus dados:</h1>
+            <h1 className='insiraDadosTexto'>Insira os seus dados:</h1>
         </div>
         <div className='estudanteEform'>
             <img src={ estudante } alt="" />
+            <div className='formElogin'>
             <div className='formularioCadastro'>
                 <form className='preencher' action="">
                     <input type="text" className='campo nome' placeholder='Nome'/>
@@ -20,6 +22,8 @@ export const Cadastro = () =>{
                     <input type="text" className='campo confirmaSenha' placeholder='Confirmar senha'/>
                 </form>
                 <button className='botaoEnviar'>Enviar</button>
+            </div>
+            <h1 className='verificaLogin'>Já possui uma conta? <Link to = '/login' className='loginTxt'>Login</Link></h1>
             </div>
         </div>
        </div>
