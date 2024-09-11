ea6b34ccd07a7d63a13ddf57495c4e6ac7c9f0bd
joaopedroa1 <joaolpq@yahoo.com>
2024-09-11 10:49:19 -0300

    style: changing the about section

--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -1,18 +1,25 @@
 import './style.css'
-import { CursoDestaque } from '../CursoDestaque'
+import { FaCheck } from "react-icons/fa";
+import mulherSobre from '../../assets/mulherSobre.png'
 
 export const About = () => {
     return(
-        <>
+        <section className='sobre'>
         <div className="invisivi"></div>
-        <div className='positivas'>
-            <h1 className="texto1Branco">
-                Destaques
-            </h1>
-            <div className="cursosDestaque">
-                <CursoDestaque></CursoDestaque>
-            </div>
+        <div className="soAqui">
+            <hgroup className='textoSoAqui'>
+                <h1 className="textoEstilo1">Só aqui você encontra</h1>
+                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Avaliações honestas</p>
+                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Melhores Cursos</p>
+                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Melhor Sistema de <br /> Pesquisa</p>
+                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Acesso Centralizado <br /> a Recursos</p>
+                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Feedback entre os <br /> alunos</p>
+                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Isso e muito mais!</p>
+            </hgroup>
+            <img className='mulherSobre' src={mulherSobre} alt="" />
         </div>
-        </>
+        <h1 className="textoEstilo1">Avaliação dos nossos alunos</h1>
+
+        </section>
     )
 }
