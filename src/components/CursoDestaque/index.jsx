1d30921d23ca442857c9413ee58aaf5b3be84d5e
joaopedroa1 <joaolpq@yahoo.com>
2024-09-11 10:50:08 -0300

    style: changing the highlited courses

--- src/components/CursoDestaque/index.jsx
+++ src/components/CursoDestaque/index.jsx
@@ -1,19 +1,24 @@
-let imagemCurso;
-let cursoTipo;
-let professorCurso;
+import PropTypes from 'prop-types';
+import './style.css'
 
 
 
-export const CursoDestaque = () => {
+export const CursoDestaque = ({imagemCurso, cursoTipo, professorCurso}) => {
     return (
         <>
         <div className="orgazinazcaoCursosDestaque">
             <div className="cursoDestaqueFoto">
                 {imagemCurso}
             </div>
-            <p>{cursoTipo}</p>
-            <p>{professorCurso}</p>
+            <p className='destaquesTexto'>{cursoTipo}</p>
+            <p className='destaquesTexto'>{professorCurso}</p>
         </div>
         </>
     )
+}
+
+CursoDestaque.propTypes = {
+    imagemCurso: PropTypes.any,
+    cursoTipo: PropTypes.any,
+    professorCurso: PropTypes.any
 }
