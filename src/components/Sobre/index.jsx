03071820a4e328e20fea76ba63d3cdc655c5e48e
joaopedroa1 <joaolpq@yahoo.com>
2024-09-12 11:09:50 -0300

    style: changing the about screen padding

--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -1,24 +1,33 @@
-import './style.css'
+import '../Sobre/style.css'
+import santana from '../../assets/santana.png'
 import { FaCheck } from "react-icons/fa";
 import mulherSobre from '../../assets/mulherSobre.png'
+import { AvaliacaoAluno } from '../AlunoAvaliacao';
 
 export const About = () => {
     return(
         <section className='sobre'>
         <div className="invisivi"></div>
+        <div className="positivas"></div>
         <div className="soAqui">
             <hgroup className='textoSoAqui'>
-                <h1 className="textoEstilo1">Só aqui você encontra</h1>
+                <h1 className="textoEstilo1 maior">Só aqui você encontra</h1>
                 <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Avaliações honestas</p>
                 <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Melhores Cursos</p>
-                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Melhor Sistema de <br /> Pesquisa</p>
-                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Acesso Centralizado <br /> a Recursos</p>
-                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Feedback entre os <br /> alunos</p>
+                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Melhor Sistema de Pesquisa</p>
+                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Acesso Centralizado a Recursos</p>
+                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Feedback entre os alunos</p>
+                <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Algoritmo eficaz e preciso</p>
                 <p className="textoSoAqui"><FaCheck size={24}></FaCheck> Isso e muito mais!</p>
             </hgroup>
             <img className='mulherSobre' src={mulherSobre} alt="" />
         </div>
-        <h1 className="textoEstilo1">Avaliação dos nossos alunos</h1>
+        <div className="padding112">
+            <h1 className="textoEstilo1">Avaliação dos nossos alunos</h1>
+            <div className='avaliacoesAlunos'>
+            <AvaliacaoAluno imagemAlunoAvAliacao={santana} nomeAlunoAvaliacao={'Gustavo Santana'} textoAlunoAvaliacao={'Passei no vestibular da FUVEST graças a vocês, obrigado!'} avaliacaoPositiva={'Avaliação positiva'}></AvaliacaoAluno>
+        </div>
+        </div>
 
         </section>
     )
