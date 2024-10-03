644dd40a51e7dbad64fe7ed3aeb9d8c41d5dfc3a
joaopedroa1 <joaolpq@yahoo.com>
2024-10-03 08:24:26 -0300

    style: making different reviews from other students

--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -4,7 +4,17 @@ import { FaCheck } from "react-icons/fa";
 import mulherSobre from '../../assets/mulherSobre.png'
 import { AvaliacaoAluno } from '../AlunoAvaliacao';
 import { CursoDestaque } from '../CursoDestaque';
-import fotoReact from '../../assets/fotoReact.png'
+import fotoReact from '../../assets/fotoReact.png';
+import lohan from '../../assets/lohan.png'
+import faraum from '../../assets/faraum.png'
+import guh from '../../assets/guh.png'
+import { LuBrush } from "react-icons/lu";
+import { Cartao } from '../Cartao'
+import { CartaoAmarelo } from '../CartaoAmarelo'
+import { GiGraduateCap } from "react-icons/gi";
+import { CartaoBranco } from '../CartaoBranco'
+import { SlBadge } from "react-icons/sl";
+
 export const About = () => {
     return(
         <section className='sobre'>
@@ -34,12 +44,21 @@ export const About = () => {
             <h1 className="textoEstilo1">Avaliação dos nossos alunos</h1>
             <div className='avaliacoesAlunos'>
             <AvaliacaoAluno imagemAlunoAvAliacao={santana} nomeAlunoAvaliacao={'Gustavo Santana'} textoAlunoAvaliacao={'Passei no vestibular da FUVEST graças a vocês, obrigado!'} avaliacaoPositiva={'Avaliação positiva'}></AvaliacaoAluno>
-            <AvaliacaoAluno imagemAlunoAvAliacao={santana} nomeAlunoAvaliacao={'Gustavo Santana'} textoAlunoAvaliacao={'Passei no vestibular da FUVEST graças a vocês, obrigado!'} avaliacaoPositiva={'Avaliação positiva'}></AvaliacaoAluno>
-            <AvaliacaoAluno imagemAlunoAvAliacao={santana} nomeAlunoAvaliacao={'Gustavo Santana'} textoAlunoAvaliacao={'Passei no vestibular da FUVEST graças a vocês, obrigado!'} avaliacaoPositiva={'Avaliação positiva'}></AvaliacaoAluno>
-            <AvaliacaoAluno imagemAlunoAvAliacao={santana} nomeAlunoAvaliacao={'Gustavo Santana'} textoAlunoAvaliacao={'Passei no vestibular da FUVEST graças a vocês, obrigado!'} avaliacaoPositiva={'Avaliação positiva'}></AvaliacaoAluno>
+            <AvaliacaoAluno imagemAlunoAvAliacao={lohan} nomeAlunoAvaliacao={'Lohan Isaque'} textoAlunoAvaliacao={'Muito bom, me ajudou muito a passar no curso que eu precisava!'} avaliacaoPositiva={'Avaliação positiva'}></AvaliacaoAluno>
+            <AvaliacaoAluno imagemAlunoAvAliacao={faraum} nomeAlunoAvaliacao={'Danilo Faraum'} textoAlunoAvaliacao={'Eu achei bom, mas acho que dava pra melhorar a estrutura do site.'} avaliacaoPositiva={'Avaliação positiva'}></AvaliacaoAluno>
+            <AvaliacaoAluno imagemAlunoAvAliacao={guh} nomeAlunoAvaliacao={'Gusthavo Andrade'} textoAlunoAvaliacao={'Passei na USP graças ao Studare, vocês são feras!'} avaliacaoPositiva={'Avaliação positiva'}></AvaliacaoAluno>
             </div>
         </div>
-
+        <div className="vantagens">
+            <h1 className="textoEstilo1">Vantagens do nosso serviço</h1>
+            <div className="cartas">
+                <div className="cartasOrganizacao">
+                <Cartao icon={<LuBrush size = {140}></LuBrush>} textoCartao={'Educação'}/>
+                <CartaoAmarelo icon = {<GiGraduateCap size={140}/>} textoCartao={'Conhecimento'} />
+                <CartaoBranco icon={<SlBadge size={140}/>} textoCartao={"Confiança"}></CartaoBranco>
+                </div>
+            </div>
+        </div>
         </section>
     )
 }
