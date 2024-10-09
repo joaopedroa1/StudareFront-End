fc5c1ce75b586869082cee0185591b50f6397cbb
joaopedroa1 <joaolpq@yahoo.com>
2024-10-09 09:02:22 -0300

    style: removing functions from the navbar

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import './style.css';
-import {FaPlus, FaUserAlt as FaUser, FaSearch} from 'react-icons/fa'
+import { FaUserAlt as FaUser, FaSearch} from 'react-icons/fa'
 import logo from '../../assets/logo.svg';
 export function Menu(){
     return(
@@ -8,11 +8,10 @@ export function Menu(){
             <Link to = '/home'><img src = {logo} alt="logoStudare" className='logo'/></Link>
             <li className = 'listaLinks'>
                 <Link to = '/categorias' className='linkNav'>Categorias </Link>
-                <Link to = '/catalogar' className='linkNav' >Encontre um curso</Link>
+                <Link to = '/catalogar' className='linkNav' >Encontrar</Link>
                 <Link to = '/sobre' className='linkNav'>Sobre</Link>
             </li>
             <div className='searchUser'>
-                <button className='botaoAvaliar'> Avalie <FaPlus className = 'add'/></button>
                 <div className='lupaUser'>
                     <FaSearch className='iconeLupa'/>
                     <Link to ='/cadastro'><FaUser className='iconeUsuario'/></Link>
