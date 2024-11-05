0d783576401578b7abb497e6e8aba8945926109d
joaopedroa1 <joaolpq@yahoo.com>
2024-11-05 10:56:01 -0300

    feat: creating the teacher´s home

--- src/components/HomeProfessor/index.jsx
+++ src/components/HomeProfessor/index.jsx
@@ -1,17 +1,55 @@
 import './style.css'
-import { fotoRenan } from '../../assets/renanSilvaDev.png'
-import { ProfInfo } from '../FotoEInfoProfessor';
+import { MenuFixo } from '../MenuFixo';
+import PropTypes from 'prop-types';
+import prof from '../../assets/renanSilvaDev.png'
+import { MeuPerfilColuna } from '../meuPerfilColuna'
+import { ProfessorColuna } from '../professorColuna';
+
+const ProfInfo = ({fotoProfessor, nomeProfessor, emailProfessor}) =>{
+    return(
+        <>
+        <div className="perfilProfessor">
+            <img className='professorFoto' src={fotoProfessor} alt="foto do professor" />
+            <h1 className="nomeProfessor">{nomeProfessor}</h1>
+            <p className='emailProfessor'>{emailProfessor}</p>
+        </div>
+        </>
+    )
+}
+
+ProfInfo.propTypes = {
+    fotoProfessor: PropTypes.any,
+    nomeProfessor: PropTypes.any,
+    emailProfessor: PropTypes.any
+}
+
 export const HomeProfessor = () =>{
     return(
         <>
-        <div className="invisivi"></div>
+        <MenuFixo></MenuFixo>
         <div className="professorHomeGrid">
             <div className="lateralProfessor">
-                <ProfInfo fotoProfessor={fotoRenan} nomeProfessor={"Renan Silva Dev"} emailProfessor={'renansilva.dev@gmail.com'}></ProfInfo>
-                <div className="alaProfessor"></div>
-                <div className="alaMeuPerfilProfessor"></div>
+                <ProfInfo fotoProfessor={prof} nomeProfessor={"Renan Silva Dev"} emailProfessor={'renansilva.dev@gmail.com'}></ProfInfo>
+                <div className="alaProfessor">
+                    <ProfessorColuna></ProfessorColuna>
+                </div>
+                <div className="alaMeuPerfilProfessor">
+                    <MeuPerfilColuna></MeuPerfilColuna>
+                </div>
             </div>
-            <div className="corpoProfessor"></div>
+            <div className="corpoProfessor">
+                <div className="textoELinha">
+                    <h1 className="dia">Bom dia professor!</h1>
+                </div>
+                <div className="linha"></div>
+                <div className='info'>
+                    <div className="quadradosInfo">
+                        <div className="classificacaoProfessor">
+                            <h1 className="avaliacaoTexto">Sua avaliação está Positiva professor!</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         </>
     )
